701462a9722f48dc9624db08e7c8a6f561bbb7b2
Justin Wong <vsajustinwong@gmail.com>
2024-11-14 01:00:33 -0800

    Added multithreading for HAR download method
    Added multithreading for faster download speeds with HAR download method.

--- har_crawler.py
+++ har_crawler.py
@@ -1,7 +1,21 @@
 import os
 import re
 import requests
+import threading
+import time
 
+THREADING_ON = True
+
+def download_image(url, filepath):
+    os.makedirs(os.path.dirname(filepath), exist_ok=True)
+
+    try:
+        with open(filepath, "wb") as f:
+            f.write(requests.get(url).content)
+    except Exception as exception:
+        print("EXCEPTION OCCURRED")
+        print(exception)
+        print(url, filepath)
 
 def har_crawl():
     # Read HAR file (`./har.json`)
@@ -9,7 +23,7 @@ def har_crawl():
     with open("har.json") as file:
         # Extract card url (check if valid) (regex)
         for line in file:
-            url = re.search(r"https:\/\/\S+card_image\/\S+.png", line)
+            url = re.search(r"https:\/\/\S+card_image\/\S+.(?:png|jpg|PNG|JPG)", line)
             if url:
                 urls.append(url.group(0))
 
@@ -22,36 +36,59 @@ def har_crawl():
     # Extract card name from each url
     # Download card and save
     # Update .txt holding card names (w/ order)
-    gen_name = re.search(r"BS-JA\/(\S+)\/", urls[0]).group(1).upper()
+    gen_name = re.search(r"card_image\/BS-JA\/(\S+)\/", urls[0]).group(1).upper()
     print(f"Processing set '{gen_name}'...")
 
     txt_save = f"{os.path.dirname(os.path.realpath(__file__))}/decks/" + gen_name + ".txt"
     txt_print = []
 
+    threads = []
     for i, url in enumerate(urls):
-        print(f"{i}/{len(urls)}")
-        card_name = re.search(gen_name + r"\/(\S+)\.png", url).group(1).upper()
+        try:
+            card_name = re.search(r"card_image\/BS-JA\/\S+\/(\S+)\.(?:png|jpg|PNG|JPG)", url).group(1).upper()
+        except AttributeError as err:
+            print(f"ERR card_name parse - cannot parse name from url: {url}, gen_name: {gen_name}")
+            raise AttributeError(err)
         if "%2B" in card_name:
             card_name = card_name[:card_name.find("%2B")]
+        if "-" not in card_name:
+            card_name = f"{gen_name}-{card_name}"
         if card_name[0].isdigit():
             card_name = "BS" + card_name
-        if card_name[-1] in ["p", "P"]:
+        card_name = card_name.replace("_", "-")
+        if card_name[-2:] in ["-D", "-d"]:
+            card_name = card_name[:-2]
+        if card_name[-1] in ["P", "p"]:
             card_name = card_name[:-1] + "-SCR"
+
         if card_name not in txt_print:
             txt_print.append(card_name)
 
         filename = "downloads/" + gen_name + "/assets/" + card_name + ".jpg"
-        os.makedirs(os.path.dirname(filename), exist_ok=True)
 
-        try:
-            with open(filename, "wb") as f:
-                f.write(requests.get(url).content)
-        except Exception as exception:
-            print("EXCEPTION OCCURRED")
-            print(exception)
-            print(url, card_name, gen_name)
+        if THREADING_ON:
+            threads.append(threading.Thread(target=download_image, args=(url, filename)))
+            threads[i].start()
+            if len(threads) % 40 == 1:
+                time.sleep(2)
+            print(f"Threads launched: {i}/{len(urls)}")
+        else:
+            print(f"{i}/{len(urls)}")
+            download_image(url, filename)
+
+    count = 0
+    while True and THREADING_ON:
+        active_threads = threading.active_count()
+        if active_threads == 1:
+            print(f"Threads done: {len(urls)}/{len(urls)}")
+            break
+        elif active_threads != count:
+            count = active_threads
+            print(f"Threads done: {len(urls) - count}/{len(urls)}")
+        time.sleep(0.1)
 
     with open(txt_save, "w") as txtFile:
+        txt_print.sort()
         for i in range(len(txt_print)):
             txtFile.write(txt_print[i])
             if i < len(txt_print) - 1:
